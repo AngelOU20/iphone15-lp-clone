be737e4e5c786f314dff7d5d9cfa4ac7bd643f61
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-23 14:59:12 -0500

    add header and hero components to Home page

---
+++ src/modules/home/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/Header.jsx";
+import { Hero } from "../components/Hero";
+
+const Home = () => {
+  return (
+    <main className="bg-black">
+      <Header />
+      <Hero />
+    </main>
+  );
+};
+
+export default Home;
