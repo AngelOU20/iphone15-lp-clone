336af7daa68d6b68c0ea8c777d04be0ce9ac02f8
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-24 18:41:51 -0500

    refactor width screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import { DesktopNav, MobileNav } from "./navbar";
 export const Header = () => {
   return (
     <header className="w-full bg-black">
-      <div className="h-11 hidden md:block">
+      <div className="max-[900px]:hidden h-10">
         <DesktopNav />
       </div>
 
-      <div className="min-[900px]:hidden">
+      <div className="min-[900px]:hidden h-10">
         <MobileNav />
       </div>
     </header>
