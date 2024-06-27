2823b19e58167099ede349f0d2afcdfea079d637
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-27 14:47:34 -0500

    add Loader

---
+++ src/modules/home/components/Loader.jsx
@@ -0,0 +1,21 @@
+import { Html } from "@react-three/drei";
+import React from "react";
+
+export const Loader = () => {
+  return (
+    <Html>
+      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+        <div className="w-[10vw] h-[10vw] rounded-full">
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+            role="status"
+          >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+              Loading...
+            </span>
+          </div>
+        </div>
+      </div>
+    </Html>
+  );
+};
