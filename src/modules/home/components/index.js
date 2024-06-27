465b4997424784c913a3a563425099d155c3a136
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-27 14:45:36 -0500

    add new exports

--- src/modules/home/components/index.js
+++ src/modules/home/components/index.js
@@ -2,3 +2,7 @@ export * from "./Hero";
 export * from "./Highlights";
 export * from "./VideoCarousel";
 export * from "./Model";
+export * from "./ModelView";
+export * from "./Lights";
+export * from "./Loader";
+// export {default as IPhone} from "./Iphone";
